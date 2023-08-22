db7902b9322f8dd85ef8bab24c9f1cf8ef9c5c39
Dylan Reimerink <dylan.reimerink@isovalent.com>
2023-08-22 15:08:54 +0200

    devices_controller: Switched devices and routes from StateDB to StateDB2
    This commit switches the devices controller over from the old StateDB
    to the new StateDB2 implementation.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/datapath/tables/device.go
+++ pkg/datapath/tables/device.go
@@ -8,44 +8,40 @@ import (
 	"net"
 	"net/netip"
 
-	"github.com/hashicorp/go-memdb"
 	"golang.org/x/exp/slices"
 
-	"github.com/cilium/cilium/pkg/statedb"
+	"github.com/cilium/cilium/pkg/statedb2"
+	"github.com/cilium/cilium/pkg/statedb2/index"
 )
 
-const (
-	DeviceNameIndex statedb.Index = "Name"
-)
+var (
+	DeviceIDIndex = statedb2.Index[*Device, int]{
+		Name: "id",
+		FromObject: func(d *Device) index.KeySet {
+			return index.NewKeySet(index.Int(d.Index))
+		},
+		FromKey: func(idx int) []byte {
+			return index.Int(idx)
+		},
+		Unique: true,
+	}
 
-// deviceTableSchema defines the table schema for the device table.
-//
-// It contains all the network devices on the local node. The
-// ones used by Cilium have 'Selected' set to true. We track all
-// devices to collect address and route information while the device
-// is perhaps not yet selected.
-//
-// Use the SelectedDevices() query function to look up and watch
-// the devices that should be used.
-var deviceTableSchema = &memdb.TableSchema{
-	Name: "devices",
-	Indexes: map[string]*memdb.IndexSchema{
-		string(statedb.IDIndex): {
-			Name:         string(statedb.IDIndex),
-			AllowMissing: false,
-			Unique:       true,
-			Indexer:      &memdb.IntFieldIndex{Field: "Index"},
+	DeviceNameIndex = statedb2.Index[*Device, string]{
+		Name: "name",
+		FromObject: func(d *Device) index.KeySet {
+			return index.NewKeySet(index.String(d.Name))
 		},
-		string(DeviceNameIndex): {
-			Name: string(DeviceNameIndex),
-			// Name can be temporarily missing if we create the device
-			// from an address update.
-			AllowMissing: true,
-			Unique:       true,
-			Indexer:      &memdb.StringFieldIndex{Field: "Name"},
+		FromKey: func(name string) []byte {
+			return index.String(name)
 		},
-	},
-}
+	}
+
+	DeviceTableCell = statedb2.NewTableCell[*Device](
+		"devices",
+		DeviceIDIndex,
+		DeviceNameIndex,
+	)
+)
 
 // Device is a local network device along with addresses associated with it.
 type Device struct {
@@ -90,36 +86,22 @@ func (d *DeviceAddress) AsIP() net.IP {
 	return d.Addr.AsSlice()
 }
 
-// DeviceByIndex constructs a query to find a device by its
-// interface index.
-func DeviceByIndex(index int) statedb.Query {
-	return statedb.Query{Index: statedb.IDIndex, Args: []any{index}}
-}
-
-// DeviceByName constructs a query to find a device by its
-// name.
-func DeviceByName(name string) statedb.Query {
-	return statedb.Query{Index: DeviceNameIndex, Args: []any{name}}
-}
-
 // SelectedDevices returns the network devices selected for
 // Cilium use.
 //
 // The invalidated channel is closed when devices have changed and
 // should be requeried with a new transaction.
-func SelectedDevices(r statedb.TableReader[*Device]) (devs []*Device, invalidated <-chan struct{}) {
-	iter, err := r.Get(statedb.Query{Index: DeviceNameIndex})
-	if err != nil {
-		// table schema is malformed?
-		panic(err)
-	}
-	for dev, ok := iter.Next(); ok; dev, ok = iter.Next() {
+func SelectedDevices(tbl statedb2.Table[*Device], rtx statedb2.ReadTxn) (devs []*Device, invalidated <-chan struct{}) {
+	iter, invalidate := tbl.All(rtx)
+	statedb2.ProcessEach(iter, func(dev *Device, _ uint64) error {
 		if !dev.Selected {
-			continue
+			return nil
 		}
 		devs = append(devs, dev)
-	}
-	return devs, iter.Invalidated()
+		return nil
+	})
+
+	return devs, invalidate
 }
 
 // DeviceNames extracts the device names from a slice of devices.
--- pkg/statedb2/index/int.go
+++ pkg/statedb2/index/int.go
@@ -3,7 +3,26 @@
 
 package index
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
+const intSize = int(unsafe.Sizeof(int(0)))
+
+func Int(n int) Key {
+	buf := make([]byte, intSize)
+	switch intSize {
+	case 4:
+		binary.BigEndian.PutUint32(buf, uint32(n))
+	case 8:
+		binary.BigEndian.PutUint64(buf, uint64(n))
+	default:
+		panic("unknown int size")
+	}
+
+	return buf
+}
 
 func Uint64(n uint64) Key {
 	buf := make([]byte, 8)
