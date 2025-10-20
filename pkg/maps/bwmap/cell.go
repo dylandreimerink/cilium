9cd26a4aac52bb84b2ad6f1119d764aa5eb42417
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-10-20 15:27:00 +0200

    maps/bwmap: use map registry
    The pkg/maps/bwmap package now uses the map registry. The compile time
    configuration for map size has been removed.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/maps/bwmap/cell.go
+++ pkg/maps/bwmap/cell.go
@@ -4,12 +4,16 @@
 package bwmap
 
 import (
+	"fmt"
+	"log/slog"
+
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/statedb"
 	"github.com/cilium/statedb/reconciler"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/maps/registry"
 )
 
 // Cell manages the cilium_throttle BPF map for implementing per-endpoint
@@ -25,32 +29,71 @@ var Cell = cell.Module(
 	cell.Provide(
 		NewEdtTable,
 		statedb.RWTable[Edt].ToTable,
-		newThrottleMap,
+		provideMap,
 	),
 	cell.Invoke(
+		createMap,
 		registerReconciler,
-		bpf.RegisterTablePressureMetricsJob[Edt, throttleMap],
+		bpf.RegisterTablePressureMetricsJob[Edt, *throttleMap],
 	),
 )
 
-func registerReconciler(cfg types.BandwidthConfig, m throttleMap, edts statedb.RWTable[Edt], params reconciler.Params) error {
-	if cfg.EnableBandwidthManager {
-		ops := bpf.NewMapOps[Edt](m.Map)
-		_, err := reconciler.Register(
-			params,
-			edts,
-			func(e Edt) Edt { return e },
-			func(e Edt, s reconciler.Status) Edt {
-				e.Status = s
-				return e
-			},
-			func(e Edt) reconciler.Status {
-				return e.Status
-			},
-			ops,
-			nil,
-		)
-		return err
+// provideMap provides a throttleMap to the Hive and configures its MapSpec in
+// the MapRegistry.
+func provideMap(cfg types.BandwidthConfig, reg *registry.MapRegistry) (out bpf.MapOut[*throttleMap], err error) {
+	if err := reg.Modify(MapName, func(m *registry.MapSpecPatch) {
+		m.MaxEntries = uint32(MapSize)
+	}); err != nil {
+		return bpf.MapOut[*throttleMap]{}, err
 	}
+
+	return bpf.NewMapOut(&throttleMap{}), nil
+}
+
+// createMap creates and opens the throttle BPF map if the bandwidth manager is
+// enabled.
+func createMap(logger *slog.Logger, cfg types.BandwidthConfig, reg *registry.MapRegistry, m *throttleMap) error {
+	if !cfg.EnableBandwidthManager {
+		// Remove map pin if the map is disabled.
+		bpf.Remove(bpf.MapPath(logger, MapName))
+
+		return nil
+	}
+
+	spec, err := reg.Get(MapName)
+	if err != nil {
+		return fmt.Errorf("getting MapSpec: %w", err)
+	}
+	m.m = bpf.NewMapFromSpec(spec, &EdtId{}, &EdtInfo{})
+
+	return m.m.OpenOrCreate()
+}
+
+// registerReconciler registers the reconciler for the bandwidth map if the map
+// has been created.
+func registerReconciler(cfg types.BandwidthConfig, m *throttleMap, edts statedb.RWTable[Edt], params reconciler.Params) error {
+	// Only register the reconciler if the map has been created.
+	if m.m == nil {
+		return nil
+	}
+
+	ops := bpf.NewMapOps[Edt](m.m)
+	if _, err := reconciler.Register(
+		params,
+		edts,
+		func(e Edt) Edt { return e },
+		func(e Edt, s reconciler.Status) Edt {
+			e.Status = s
+			return e
+		},
+		func(e Edt) reconciler.Status {
+			return e.Status
+		},
+		ops,
+		nil,
+	); err != nil {
+		return fmt.Errorf("registering bandwidth edt reconciler: %w", err)
+	}
+
 	return nil
 }
